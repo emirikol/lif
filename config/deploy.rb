b8b5ba315bb02bb3b051e312da6f19a1e22cc54b
Daniel Davis <shoefish@gmail.com>
2020-08-01 17:59:28 +0300

    puma & ws config

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ append :linked_files, "config/master.key", "db/production.sqlite3"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-append :linked_dirs, '.bundle', 'tmp/pids'
+append :linked_dirs, '.bundle', 'tmp/pids', 'tmp/sockets'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
